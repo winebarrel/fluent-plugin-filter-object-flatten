aa3ee3eb57e97ddd67abff2930f42e77947d04ae
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-10-30 13:21:37 +0900

    tweak for invalid pattern error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,6 @@ tr #{options[:tr].inspect}
   tag = options[:tag] || 'test.default'
   Fluent::Test::FilterTestDriver.new(Fluent::ObjectFlattenFilter, tag).configure(fluentd_conf)
 end
+
+# prevent Test::Unit's AutoRunner from executing during RSpec's rake task
+Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
